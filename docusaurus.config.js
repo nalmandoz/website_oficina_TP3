cd0056970e9bc7ac671c26be3623249608466cc0
nalmandoz <a79370@ualg.pt>
2023-12-21 19:43:54 +0000

    Revert "Configurações iniciais"
    This reverts commit 7ebc4155344bd41536b6ff9afd5559a28939ee18.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,20 +6,20 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'oficina automovel de Faro',
-  tagline: 'revisões, arranjo e aconcelhamento proficional',
+  title: 'My Test Site',
+  tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://github.com/',
+  url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/websiteoficina/',
+  baseUrl: '/docusaurus-ghpages/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Vasco_Victor_Nicolas_Jazmin', // Usually your GitHub org/user name.
-  projectName: 'website_oficina', // Usually your repo name.
+  organizationName: 'masaleiro', // Usually your GitHub org/user name.
+  projectName: 'docusaurus-ghpages', // Usually your repo name.
   deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
@@ -29,8 +29,8 @@ const config = {
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'pt',
-    locales: ['pt'],
+    defaultLocale: 'en',
+    locales: ['en'],
   },
 
   presets: [
