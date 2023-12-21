6a409b95c081635bb0c7b71e92027cd031c2aa7f
a85242 <a85242@ualg.pt>
2023-12-21 22:53:55 +0000

    label Tutorial na barra de navegação agora chama-se Info

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -75,7 +75,7 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Info',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
